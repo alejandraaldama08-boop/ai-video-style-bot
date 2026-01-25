54dd0d6ba25c08d5ec5f9aa94ba8940ff0f84870
alejandraaldama08-boop <alejandraaldama08@gmail.com>
2026-01-25 14:28:54 +0100

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,4 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+export const BACKEND_URL = "https://ai-video-style-bot-3.onrender.com";
